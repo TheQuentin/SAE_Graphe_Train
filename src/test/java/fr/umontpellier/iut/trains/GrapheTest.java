8967e7f97331b25e1fa3c7cd3473be47e4a0c1ea
Petru Valicov <petru.valicov@umontpellier.fr>
2024-05-16 13:53:26 +0200

    correction tests plateaux sans tuiles mer

--- src/test/java/fr/umontpellier/iut/trains/GrapheTest.java
+++ src/test/java/fr/umontpellier/iut/trains/GrapheTest.java
@@ -1,12 +1,13 @@
 package fr.umontpellier.iut.trains;
 
-import fr.umontpellier.iut.graphes.Graphe;
-import fr.umontpellier.iut.trains.plateau.Plateau;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Timeout;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import fr.umontpellier.iut.graphes.Graphe;
+import fr.umontpellier.iut.trains.plateau.Plateau;
 
 @Timeout(value = 1, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
 public class GrapheTest {
@@ -15,8 +16,8 @@ public class GrapheTest {
         Jeu jeu = new Jeu(new String[]{"Batman", "Robin"}, new String[]{}, Plateau.TOKYO);
         Graphe graphe = jeu.getGraphe();
 
-        assertEquals(76, graphe.getNbSommets());
-        assertEquals(194 - 7, graphe.getNbAretes()); // 194 arêtes entre hexagones, 7 barrières
+        assertEquals(66, graphe.getNbSommets());
+        assertEquals(156, graphe.getNbAretes());
         assertTrue(graphe.estConnexe());
         assertTrue(graphe.possedeUnCycle());
         assertEquals(6, graphe.degreMax());
@@ -24,12 +25,14 @@ public class GrapheTest {
 
     @Test
     public void test_distances_tokyo() {
-        Jeu jeu = new Jeu(new String[]{"Batman", "Robin"}, new String[]{}, Plateau.TOKYO);
+        Jeu jeu = new Jeu(new String[]{"Rick", "Morty"}, new String[]{}, Plateau.TOKYO);
         Graphe graphe = jeu.getGraphe();
 
-        assertEquals(8, graphe.getDistance(graphe.getSommet(0), graphe.getSommet(54)));
+        assertEquals(4, graphe.getDistance(graphe.getSommet(0), graphe.getSommet(54)));
         assertEquals(0, graphe.getDistance(graphe.getSommet(13), graphe.getSommet(54)));
-        assertEquals(8, graphe.getDistance(graphe.getSommet(67), graphe.getSommet(9)));
+        assertEquals(0, graphe.getDistance(graphe.getSommet(3), graphe.getSommet(54)));
+        assertEquals(11, graphe.getDistance(graphe.getSommet(67), graphe.getSommet(9)));
+        assertEquals(2, graphe.getDistance(graphe.getSommet(34), graphe.getSommet(35)));
     }
 
     @Test
@@ -37,8 +40,8 @@ public class GrapheTest {
         Jeu jeu = new Jeu(new String[]{"Lois", "Clark"}, new String[]{}, Plateau.OSAKA);
         Graphe graphe = jeu.getGraphe();
 
-        assertEquals(76, graphe.getNbSommets());
-        assertEquals(194 - 11, graphe.getNbAretes()); // 194 arêtes entre hexagones, 11 barrières
+        assertEquals(66, graphe.getNbSommets());
+        assertEquals(151, graphe.getNbAretes());
         assertTrue(graphe.estConnexe());
         assertTrue(graphe.possedeUnCycle());
         assertEquals(6, graphe.degreMax());
